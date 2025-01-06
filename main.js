ed0a33abf880167bc780ddeab17ccf5d8bf9933f
myusermouse <mdrahulsarkar5525@gmail.com>
2025-01-06 16:18:38 +0600

    update

--- main.js
+++ main.js
@@ -4,9 +4,11 @@ let searchIcon = document.getElementById('search'),
  searchIcon.addEventListener('click',()=>{
      searchInput.style.width = "200px";
      searchIcon.src = "./image/close.png";
-     searchIcon.classList.add('search-close');
  });
- 
+ searchInput.addEventListener('click',()=>{
+     searchInput.style.width = "0px";
+     searchIcon.src = "./image/search.png";
+ })
 
  let dodMe = document.getElementById('dod'),
     dropdownLeft = document.getElementById('left-dropdown'),
@@ -27,4 +29,36 @@ let searchIcon = document.getElementById('search'),
  /* Its very hard  */
  
  
- 
+let addNewDitails = document.getElementById('add-new-ditails'),
+    newTextAdd = document.getElementById('new-text-add');
+    
+    newTextAdd.addEventListener('click',()=>{
+        addNewDitails.style.height = "100%";
+    });
+    
+    let cancenBtn = document.getElementById('cancel').addEventListener('click',()=>{
+        addNewDitails.style.height = "0px";
+    })
+    
+    
+    /* add content */
+    
+    let addContent = document.getElementById('add-content');
+    function savedata() {
+        let nameInput = document.getElementById('name'),
+            villageInput = document.getElementById('village'),
+            notPayedIn = document.getElementById('not-payed'),
+            payedInput = document.getElementById('payed');
+            
+            
+            let section = document.createElement('section');
+                addContent.append(section);
+           console.log(nameInput.value, villageInput.value, notPayedIn.value, payedInput.value);
+            
+            nameInput.value = "";
+            villageInput.value = "";
+            notPayedIn.value = "";
+            payedInput.value = "";
+            addNewDitails.style.height = "0px";
+            
+    }
